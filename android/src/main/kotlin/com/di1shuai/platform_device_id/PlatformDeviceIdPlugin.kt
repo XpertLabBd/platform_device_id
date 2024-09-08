adc0e24b579f69216f08c87655284ae8b4154495
XpertLabBd <xdevs2011@gmail.com>
2024-09-08 15:07:03 +0600

    upload project

---
+++ android/src/main/kotlin/com/di1shuai/platform_device_id/PlatformDeviceIdPlugin.kt
@@ -0,0 +1,39 @@
+package com.di1shuai.platform_device_id
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry.Registrar
+
+public class PlatformDeviceIdPlugin: FlutterPlugin, MethodCallHandler {
+  private lateinit var channel : MethodChannel
+
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "platform_device_id")
+    channel.setMethodCallHandler(this);
+  }
+
+  companion object {
+    @JvmStatic
+    fun registerWith(registrar: Registrar) {
+      val channel = MethodChannel(registrar.messenger(), "platform_device_id")
+      channel.setMethodCallHandler(PlatformDeviceIdPlugin())
+    }
+  }
+
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    if (call.method == "getPlatformVersion") {
+      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else {
+      result.notImplemented()
+    }
+  }
+
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
+}
